7ded72b67c001e869938c80f4d5de0181dca0a47
charlie <coragano@gmail.com>
2012-11-05 15:31:00 -0500

    Added osc messages and json settings to app

--- src/PulseSenseApp.cpp
+++ src/PulseSenseApp.cpp
@@ -5,6 +5,8 @@
 #include "cinder/Font.h"
 
 #include "PulseReader.h"
+#include "OscSender.h"
+#include "cinder/Json.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -22,6 +24,11 @@ class PulseSenseApp : public AppBasic {
     void onFingerOver();
     void onFingerOut();
     
+    // OSC
+    osc::Sender     mOscSender;
+    string          mOscHost;
+    int             mOscPort;
+    
     // PULSE
     PulseReader     mPulseReader;
     
@@ -36,6 +43,28 @@ void PulseSenseApp::setup()
     gl::enableAlphaBlending();
     
     mPulseReader.setup();
+    
+    mOscHost = "localhost";
+    mOscPort = 12345;
+    
+    // SETUP JSON
+    JsonTree js;
+    fs::path jsonPath = ci::app::getAppPath().parent_path() /= fs::path( "/settings.json" );
+    if( !fs::exists( jsonPath ) ){
+        console() << "PulseSense :: Cannot find settings.json file.  Creating one." << endl;
+        js.pushBack( JsonTree("osc_host", mOscHost) );
+        js.pushBack( JsonTree("osc_port", mOscPort) );
+        js.write( jsonPath );
+    }else{
+        js = JsonTree( ci::loadFile( jsonPath ) );
+        
+        mOscHost = js["osc_host"].getValue();
+        mOscPort = ci::fromString<int>( js["osc_port"].getValue() );
+    }
+    
+    console() << "PulseSense :: Osc running on " << mOscHost << ":" << mOscPort << endl;
+    
+    mOscSender.setup( mOscHost, mOscPort );
     
     eventStr = "";
     mHeartAlpha = 0;
@@ -89,24 +118,48 @@ void PulseSenseApp::onHeartBeat()
 {
     mHeartAlpha = 1.0f;
     timeline().apply( &mHeartAlpha, 1.0f, 0.0f, 1.0f );
+    
+    osc::Message msg;
+    msg.setAddress("pulse/beat");
+    msg.addIntArg( mPulseReader.getBpm() );
+    
+    mOscSender.sendMessage( msg );
 }
 
 void PulseSenseApp::onPulseStart()
 {
     eventStr = "Pulse start";
     timeline().apply( &mEventAlpha, 1.0f, 0.0f, 3.0f );
+    
+    osc::Message msg;
+    msg.setAddress("pulse/start");
+    msg.addIntArg( mPulseReader.getBpm() );
+    
+    mOscSender.sendMessage( msg );
 }
 
 void PulseSenseApp::onFingerOver()
 {
     eventStr = "Finger over";
     timeline().apply( &mEventAlpha, 1.0f, 0.0f, 3.0f );
+    
+    osc::Message msg;
+    msg.setAddress("pulse/fingerOver");
+    msg.addIntArg( 1 );
+    
+    mOscSender.sendMessage( msg );
 }
 
 void PulseSenseApp::onFingerOut()
 {
     eventStr = "Finger out";
     timeline().apply( &mEventAlpha, 1.0f, 0.0f, 3.0f );
+    
+    osc::Message msg;
+    msg.setAddress("pulse/fingerOut");
+    msg.addIntArg( 0 );
+    
+    mOscSender.sendMessage( msg );
 }
 
 
